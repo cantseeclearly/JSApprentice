1de877191a16ada207a9155ac3826648f53a6ad3
cj1 <cj1@winton.com.tw>
2020-08-18 14:41:17 +0800

    add Day51-52

---
+++ Day51-52/js/index.js
@@ -0,0 +1,48 @@
+axios.defaults.headers.common['Authorization'] = 'Bearer FGNxSHqDPDeR60YZ1FyxjchT85ogleljBY4V81WNVreVQRjDfXWoL5ElvAu';
+axios.defaults.headers.common['accept'] = 'application/json';
+
+const url = 'https://challenge.thef2e.com/api/thef2e2019/stage6/rooms';
+let select = document.querySelector('#roomType');
+let ul = document.querySelector('#roomInfo');
+
+let api = {
+    allRoom: () => {
+        axios.get(url)
+    },
+    roomInfo: (id) => {
+        axios.get(`https://challenge.thef2e.com/api/thef2e2019/stage6/room/${id}`)
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+}
+
+axios.get(url)
+    .then(res => {
+        if(res.data) {
+            console.log(res.data);
+            if(res.data.success) console.log('取得房型資訊');
+            if(res.data.items) {
+                let firstId = '';
+                let opt = document.createElement('option');
+                opt.value = ''; opt.text = '請選擇';
+                select.appendChild(opt);
+                res.data.items.forEach((i, idx) => {
+                    opt = document.createElement('option');
+                    opt.value = i.id;
+                    opt.text = i.name;
+                    console.log(opt);
+                    select.appendChild(opt);
+                });
+            }
+            //set first room info
+        }
+    });
+
+select.addEventListener('change', (e) => {
+    let path = location.pathname.replace('index', 'roomInfo');
+    location.href = `${path}?id=${e.target.value}`;
+});
---
+++ Day51-52/js/roomInfo.js
@@ -0,0 +1,38 @@
+axios.defaults.headers.common['Authorization'] = 'Bearer FGNxSHqDPDeR60YZ1FyxjchT85ogleljBY4V81WNVreVQRjDfXWoL5ElvAu';
+axios.defaults.headers.common['accept'] = 'application/json';
+
+const url = 'https://challenge.thef2e.com/api/thef2e2019/stage6/room/';
+let ul = document.querySelector('#roomInfo');
+let id = location.search.split('=')[1];
+
+window.onload = function() {
+axios.get(`${url}${id}`)
+    .then(res => {
+        console.log(res);
+        if(res.data.success) 
+        {
+            room = res.data.room;
+            let str = '';
+            room.forEach(i => {
+                str += `
+                    <li class='li'>
+                        <div>id:${i.id}</div>\n
+                        <div>name:${i.name}</div>\n
+                        <div>holidayPrice:${i.holidayPrice}</div>\n
+                        <div>normalDayPrice:${i.normalDayPrice}</div>\n
+                        <div><img src='${i.imageUrl}' alt='${i.name}' width=300 height=300 /></div>\n
+                    </li>`;
+            });
+            ul.innerHTML = str;
+        }
+    })
+    .catch(err => {
+        console.log(err);
+    })
+}
+
+function goHome() {
+    let path = location.pathname.replace('roomInfo', 'index');
+    location.href = path;
+}
+
